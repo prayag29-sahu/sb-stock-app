134daff57103bc4c5bb749513b5380f0f107585d
Prayag Sahu <sahuprayag229@gmail.com>
2026-03-04 12:54:39 +0530

    connect backend

--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import '../styles/Users.css'
-import axios from 'axios'
 import axiosInstance from '../components/axiosInstance'
 
 const Users = () => {
