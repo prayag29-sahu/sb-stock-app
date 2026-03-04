c81bc6c96b856c88142a8b314285ec884eeced6d
Prayag Sahu <sahuprayag229@gmail.com>
2026-03-04 13:58:12 +0530

    connect backend

--- client/src/components/axiosInstance.js
+++ client/src/components/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL,
   withCredentials: true,
 });
 
