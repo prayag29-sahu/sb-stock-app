78dbaeba76aa7eb85f8540be830eee635786d288
Prayag Sahu <sahuprayag229@gmail.com>
2026-03-04 13:44:59 +0530

    build logs

--- client/src/pages/AllTransactions.jsx
+++ client/src/pages/AllTransactions.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
 import '../styles/AllTransactions.css'
 import axiosInstance from '../components/axiosInstance';
--- client/src/pages/StockChart.jsx
+++ client/src/pages/StockChart.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
 import '../styles/StockChart.css'
 import Chart from 'react-apexcharts'
